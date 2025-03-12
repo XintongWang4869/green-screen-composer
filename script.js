f5d15460f467866d22fc6fac76af82c9296fcb2f
XintongWang4869 <xintong3248@gmail.com>
2025-03-12 11:13:41 +0800

    add function for resizing images

--- script.js
+++ script.js
@@ -9,28 +9,60 @@ var output_canvas = document.getElementById("output-canvas");
 
 function upload_img(id) {
     // alert('ran upload_img');
-    
+
     if (id === "fg-img") {
         // alert("fg loading...");
-        
+
         fg_img = new SimpleImage(document.getElementById("fg-img"));
         fg_img.drawTo(fg_canvas);
         // alert("fg loaded");
     }
     if (id === "bg-img") {
         bg_img = new SimpleImage(document.getElementById("bg-img"));
-        bg_img.drawTo(bg_canvas);       
+        bg_img.drawTo(bg_canvas);
     }
 
 }
 
 
+function resize(img_canvas, newWidth, newHeight) {
+    let canvas = document.createElement("canvas");
+    let ctx = canvas.getContext("2d");
+    canvas.width = newWidth;
+    canvas.height = newHeight;
+    ctx.drawImage(img_canvas, 0, 0, newWidth, newHeight);
+    let new_img = new SimpleImage(canvas);
+    return new_img;
+}
+
 function composite() {
-    if (output) {
-        alert("Please clear the images.")
+    // if (output) {
+    //     alert("Please clear the images.")
+    // }
+    if (fg_img == null || bg_img == null) {
+        alert("Please upload images first!");
     }
-    if (fg_img && bg_img) {
-        // alert("images exist!");
+
+    else {
+        // resize the images:
+        let fg_w = fg_img.getWidth();
+        let fg_h = fg_img.getHeight();
+        let bg_w = bg_img.getWidth();
+        let bg_h = bg_img.getHeight();
+        if (fg_w <= bg_w && fg_h <= bg_h) {
+            // resize bg
+            bg_img = resize(bg_canvas, fg_w, fg_h);
+        }
+        else if (fg_w > bg_w && fg_h > bg_h) {
+            // resize fg
+            fg_img = resize(fg_canvas, bg_w, bg_h);
+        }
+
+        else {
+            alert("The uploaded images have very different size. Please try other images.")
+        }
+
+        // composite 
         var output = new SimpleImage(fg_img.getWidth(), fg_img.getHeight());
         for (var pixel of fg_img.values()) {
             var x = pixel.getX();
@@ -46,15 +78,16 @@ function composite() {
         }
         output.drawTo(output_canvas)
         // alert("finish!");
+
+
     }
-    else {
-        alert("Please upload images first!");
-    }
+
+
 }
 
 function pass_green_threshold(pixel) {
 
-    if (pixel.getGreen() >= pixel.getRed()*1.5 &&  pixel.getGreen() >= pixel.getBlue()*1.25) {
+    if (pixel.getGreen() >= pixel.getRed() * 1.5 && pixel.getGreen() >= pixel.getBlue() * 1.25) {
         return true;
     }
     return false;
